b6971cae9dbfa0bca6c0a513e0bcfe4d25c91f95
ryme07 <remydelrieu@hotmail.fr>
2020-11-05 18:50:12 +0100

    feat:animation duration Home

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -5,8 +5,10 @@ import { Fade } from "react-awesome-reveal";
 export default function Home() {
   return (
     <div className="component first-component">
-      <Fade direction="left">
+      <Fade direction="left" duration={2000}>
         <h2>First Component</h2>
+      </Fade>
+      <Fade direction="right" duration={1000}>
         <Card />
       </Fade>
     </div>

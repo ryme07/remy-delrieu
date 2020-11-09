035e1ed4a2046ce058b86ac7fab5f8c0ea77af77
ryme07 <remydelrieu@hotmail.fr>
2020-11-09 18:08:27 +0100

    feat: add scss variables

--- src/Views/Project.js
+++ src/Views/Project.js
@@ -1,12 +1,28 @@
 import React from "react";
 import Card from "../Components/Card";
 import { Fade } from "react-awesome-reveal";
+import "../Style/Project.scss";
 
 export default function Project() {
   return (
     <div className="component first-component">
       <Fade direction="left" duration={2000}>
-        <h2>First Component</h2>
+        <div className="project">
+          <h2>DSI POLE EMPLOI</h2>
+          <h5>JRAD V2</h5>
+          <div className="block">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+              maximus, nulla ut commodo sagittis, sapien dui mattis dui, non
+              pulvinar lorem felis nec erat
+            </p>
+          </div>
+          <ul className="techno">
+            <li>Angular</li>
+            <li>Jenkins</li>
+            <li>SonarQube</li>
+          </ul>
+        </div>
       </Fade>
       <Fade direction="right" duration={1000}>
         <Card />

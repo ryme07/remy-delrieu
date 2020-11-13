8148353630f1e940ce30cec3be11183c948dbff5
ryme07 <remydelrieu@hotmail.fr>
2020-11-13 13:09:44 +0100

    feat: new version of project rendering in App.js

--- src/App.js
+++ src/App.js
@@ -4,76 +4,101 @@ import ReactPageScroller from "react-page-scroller";
 import Home from "./Views/Home";
 import Project from "./Views/Project";
 import About from "./Views/About";
-import { Zoom } from "react-awesome-reveal";
-import Card from "./Components/Card";
+// import { Zoom } from "react-awesome-reveal";
+// import Card from "./Components/Card";
 
 function App() {
-  // const ComponentOne = () => <div>Hello world!</div>;
-  // const ComponentTwo = () => (
-  //   <div>
-  //     <div>Hola el mundo!</div>
-  //     <ComponentThree foo={<ComponentOne />} />
-  //   </div>
-  // );
-  // const ComponentThree = ({ foo }) => <div>{foo}</div>;
+  const ComponentOne = () => <div>Hello world!</div>;
+  const ComponentTwo = () => (
+    <div>
+      <div>Hola el mundo!</div>
+      <ComponentThree foo={<ComponentOne />} />
+    </div>
+  );
+  const ComponentThree = ({ foo }) => <div>{foo}</div>;
 
-  const WrapperPE = () => {
-    return (
-      <div className="wrapper">
+  // const WrapperPE = () => {
+  //   return (
+  //     <div className="wrapper">
+  //       <Project
+  //         title="DSI POLE EMPLOI"
+  //         subtitle="JRAD V2"
+  //         framework="Angular"
+  //         secondtechno="Jenkins"
+  //         thirdtechno="SonarQube"
+  //       />
+  //       <Zoom direction="bottom" delay={1000} duration={2000}>
+  //         <Card hello="PE PROJECT" />
+  //       </Zoom>
+  //     </div>
+  //   );
+  // };
+
+  // const WrapperBC = () => {
+  //   return (
+  //     <div className="wrapper">
+  //       <Project
+  //         title="BEWARE CYBERLABS"
+  //         subtitle="QUIZ APP"
+  //         framework="VueJs"
+  //         secondtechno="NodeJs"
+  //         thirdtechno="TypeOrm"
+  //       />
+  //       <Zoom direction="bottom" delay={1000} duration={2000}>
+  //         <Card hello="BEWARE PROJECT" />
+  //       </Zoom>
+  //     </div>
+  //   );
+  // };
+
+  // const WrapperWP = () => {
+  //   return (
+  //     <div className="wrapper">
+  //       <Project
+  //         title="WEB PROJECTS"
+  //         subtitle="HACKASTAR"
+  //         framework="Angular"
+  //         secondtechno="NodeJS"
+  //         thirdtechno="Typescript"
+  //       />
+  //       <Zoom direction="bottom" delay={1000} duration={2000}>
+  //         <Card hello="web project" />
+  //       </Zoom>
+  //     </div>
+  //   );
+  // };
+  return (
+    <React.Fragment>
+      <ReactPageScroller>
+        <Home />
         <Project
           title="DSI POLE EMPLOI"
           subtitle="JRAD V2"
           framework="Angular"
           secondtechno="Jenkins"
           thirdtechno="SonarQube"
+          hello={<ComponentTwo />}
         />
-        <Zoom direction="bottom" delay={1000} duration={2000}>
-          <Card hello="PE PROJECT" />
-        </Zoom>
-      </div>
-    );
-  };
-
-  const WrapperBC = () => {
-    return (
-      <div className="wrapper">
         <Project
           title="BEWARE CYBERLABS"
           subtitle="QUIZ APP"
           framework="VueJs"
           secondtechno="NodeJs"
           thirdtechno="TypeOrm"
+          hello="BEWARE"
         />
-        <Zoom direction="bottom" delay={1000} duration={2000}>
-          <Card hello="BEWARE PROJECT" />
-        </Zoom>
-      </div>
-    );
-  };
 
-  const WrapperWP = () => {
-    return (
-      <div className="wrapper">
         <Project
           title="WEB PROJECTS"
           subtitle="HACKASTAR"
           framework="Angular"
           secondtechno="NodeJS"
           thirdtechno="Typescript"
+          hello="WEB PROJECT"
         />
-        <Zoom direction="bottom" delay={1000} duration={2000}>
-          <Card hello="web project" />
-        </Zoom>
-      </div>
-    );
-  };
-  return (
-    <React.Fragment>
-      <ReactPageScroller>
-        <Home />
-        <WrapperPE className="wrapper" />
+        {/* <WrapperPE className="wrapper" />
         <WrapperBC className="wrapper" />
-        <WrapperWP className="wrapper" />
+        <WrapperWP className="wrapper" /> */}
         <About />
       </ReactPageScroller>
     </React.Fragment>
--- src/Views/Project.js
+++ src/Views/Project.js
@@ -1,18 +1,30 @@
 import React from "react";
-import Card from "../Components/Card";
 import { Fade, Flip, Zoom } from "react-awesome-reveal";
 import "../Style/Project.scss";
+import { useSpring, animated } from "react-spring";
 
-export default function Project(props, ...cards) {
+export default function Project(propsproject) {
+  const calc = (x, y) => [
+    -(y - window.innerHeight / 2) / 20,
+    (x - window.innerWidth / 2) / 20,
+    1.1,
+  ];
+  const trans = (x, y, s) =>
+    `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
+
+  const [props, set] = useSpring(() => ({
+    xys: [0, 0, 1],
+    config: { mass: 5, tension: 350, friction: 40 },
+  }));
   return (
-    <div {...props} className="componentProject firstProject">
+    <div {...propsproject} className="componentProject firstProject">
       <div className="project">
         <Fade direction="left">
-          <h2>{props.title}</h2>
+          <h2>{propsproject.title}</h2>
         </Fade>
 
         <Fade direction="top" delay={1500} duration={2500}>
-          <h5 className="subtitle">{props.subtitle}</h5>
+          <h5 className="subtitle">{propsproject.subtitle}</h5>
         </Fade>
         <Fade direction="left" delay={700}>
           <div className="block">
@@ -25,12 +37,22 @@ export default function Project(props, ...cards) {
         </Fade>
         <Flip direction="vertical" delay={1600} duration={1000}>
           <ul className="techno">
-            <li>{props.framework}</li>
-            <li>{props.secondtechno}</li>
-            <li>{props.thirdtechno}</li>
+            <li>{propsproject.framework}</li>
+            <li>{propsproject.secondtechno}</li>
+            <li>{propsproject.thirdtechno}</li>
           </ul>
         </Flip>
       </div>
+      <Zoom direction="bottom" delay={1000} duration={2000}>
+        <animated.div
+          className="card"
+          onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+          onMouseLeave={() => set({ xys: [0, 0, 1] })}
+          style={{ transform: props.xys.interpolate(trans) }}
+        >
+          {propsproject.hello}
+        </animated.div>
+      </Zoom>
     </div>
   );
 }

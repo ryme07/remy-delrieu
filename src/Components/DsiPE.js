db349944e503b70613ec6e516859154f05d70aed
ryme07 <remydelrieu@hotmail.fr>
2020-11-17 16:32:17 +0100

    feat: add animation dsi

--- src/Components/DsiPE.js
+++ src/Components/DsiPE.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../Style/DsiPE.scss";
+import { Zoom } from "react-awesome-reveal";
 
 export default function DsiPE() {
   const getCurrentDate = () => {
@@ -14,97 +15,102 @@ export default function DsiPE() {
 
   return (
     <div className="dsi">
-      <div className="nav">
-        <ul>
-          <li>PROJECT 1</li>
-          <li>PROJECT 2</li>
-          <li>PROJECT 3</li>
-          <li>PROJECT 4</li>
-          <li>PROJECT 5</li>
-          <li>PROJECT 6</li>
-        </ul>
-      </div>
+      <Zoom direction="bottom" delay={1000} duration={2000}>
+        <div className="nav">
+          <ul>
+            <li>PROJECT 1</li>
+            <li>PROJECT 2</li>
+            <li>PROJECT 3</li>
+            <li>PROJECT 4</li>
+            <li>PROJECT 5</li>
+            <li>PROJECT 6</li>
+          </ul>
+        </div>
+      </Zoom>
       <div className="main-container-dsi">
-        <div className="left">
-          <div className="container-left">
-            <div className="build">
-              <p>THING TESTED</p>
-              <button>BUILD</button>
+        <Zoom direction="bottom" delay={1500} duration={2000}>
+          <div className="left">
+            <div className="container-left">
+              <div className="build">
+                <p>THING TESTED</p>
+                <button>BUILD</button>
+              </div>
+
+              <div className="release">
+                <p>{getCurrentDate()}</p>
+                <button>RELEASE</button>
+              </div>
+
+              <div className="tests">
+                <p>N° BUILD</p>
+                <div className="btn-tests">
+                  <button>TU</button>
+                  <button>TI</button>
+                  <button>TS</button>
+                </div>
+              </div>
             </div>
 
-            <div className="release">
-              <p>{getCurrentDate()}</p>
-              <button>RELEASE</button>
+            <div className="container-left">
+              <div className="build">
+                <p>THING TESTED</p>
+                <button className="green">BUILD</button>
+              </div>
+
+              <div className="release">
+                <p>{getCurrentDate()}</p>
+                <button className="red">RELEASE</button>
+              </div>
+
+              <div className="tests">
+                <p>N° BUILD</p>
+                <div className="btn-tests">
+                  <button className="red">TU</button>
+                  <button className="orange">TI</button>
+                  <button className="red">TS</button>
+                </div>
+              </div>
             </div>
 
-            <div className="tests">
-              <p>N° BUILD</p>
-              <div className="btn-tests">
-                <button>TU</button>
-                <button>TI</button>
-                <button>TS</button>
+            <div className="container-left">
+              <div className="build">
+                <p>THING TESTED</p>
+                <button className="green">BUILD</button>
+              </div>
+
+              <div className="release">
+                <p>{getCurrentDate()}</p>
+                <button className="red">RELEASE</button>
+              </div>
+              <div className="tests">
+                <p>N° BUILD</p>
+                <div className="btn-tests">
+                  <button className="red">TU</button>
+                  <button className="orange">TI</button>
+                  <button className="red">TS</button>
+                </div>
               </div>
             </div>
           </div>
+        </Zoom>
 
-          <div className="container-left">
-            <div className="build">
-              <p>THING TESTED</p>
-              <button className="green">BUILD</button>
+        <Zoom direction="bottom" delay={1500} duration={2000}>
+          <div className="right">
+            <div className="container-right">
+              <p>JOB</p>
+              <p className="green">TU : TOTAL : 2 SKIP: 0 FAILED</p>
+              <p className="orange">TU : TOTAL: 2 SKIP: 2 FAILED: 0</p>
+              <p className="red">TS: TOTAL: 2 SKIP: 0 FAILED: 1</p>
+              <p>
+                LAST COMMIT : {getCurrentDate()} <br />
+                SOME DATA FROM GITHUB
+              </p>
             </div>
-
-            <div className="release">
-              <p>{getCurrentDate()}</p>
-              <button className="red">RELEASE</button>
-            </div>
-
-            <div className="tests">
-              <p>N° BUILD</p>
-              <div className="btn-tests">
-                <button className="red">TU</button>
-                <button className="orange">TI</button>
-                <button className="red">TS</button>
-              </div>
+            <div className="btn-sonar">
+              <button>SONARQUBE</button>
             </div>
           </div>
-
-          <div className="container-left">
-            <div className="build">
-              <p>THING TESTED</p>
-              <button className="green">BUILD</button>
-            </div>
-
-            <div className="release">
-              <p>{getCurrentDate()}</p>
-              <button className="red">RELEASE</button>
-            </div>
-            {/* hehre */}
-            <div className="tests">
-              <p>N° BUILD</p>
-              <div className="btn-tests">
-                <button className="red">TU</button>
-                <button className="orange">TI</button>
-                <button className="red">TS</button>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="right">
-          <div className="container-right">
-            <p>JOB</p>
-            <p className="green">TU : TOTAL : 2 SKIP: 0 FAILED</p>
-            <p className="orange">TU : TOTAL: 2 SKIP: 2 FAILED: 0</p>
-            <p className="red">TS: TOTAL: 2 SKIP: 0 FAILED: 1</p>
-            <p>
-              LAST COMMIT : {getCurrentDate()} <br />
-              SOME DATA FROM GITHUB
-            </p>
-          </div>
-          <div className="btn-sonar">
-            <button>SONARQUBE</button>
-          </div>
-        </div>
+        </Zoom>
       </div>
     </div>
   );

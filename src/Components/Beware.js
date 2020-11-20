c13b1a8996edd2a36f3fce680f838c97bb31d360
ryme07 <remydelrieu@hotmail.fr>
2020-11-20 15:56:36 +0100

    feat: beware style done

--- src/Components/Beware.js
+++ src/Components/Beware.js
@@ -4,6 +4,18 @@ import "../Style/Beware.scss";
 export default function Beware() {
   return (
     //https://s3-us-west-2.amazonaws.com/s.cdpn.io/85486/wave.svg
-    <div className="beware-container">hello</div>
+    <div className="beware-container">
+      <div className="title-question">QUESTION ?</div>
+      <div className="beware-bottom">
+        <div className="beware-answers">
+          <div>ANSWER 1</div>
+          <div>ANSWER 2</div>
+          <div>ANSWER 3</div>
+        </div>
+        <div className="beware-validate">
+          <button>VALIDATE</button>
+        </div>
+      </div>
+    </div>
   );
 }

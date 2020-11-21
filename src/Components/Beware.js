eb600a9a09bee808651b5d75dcf2fba6e434160e
ryme07 <remydelrieu@hotmail.fr>
2020-11-21 14:27:43 +0100

    feat: edit beware component with map iterator

--- src/Components/Beware.js
+++ src/Components/Beware.js
@@ -2,16 +2,20 @@ import React from "react";
 import "../Style/Beware.scss";
 
 export default function Beware() {
+  const data = [
+    { name: "ANSWER 1" },
+    { name: "ANSWER 2" },
+    { name: "ANSWER 3" },
+  ];
+  const listAnswers = data.map((answer) => (
+    <div key={answer.name}>{answer.name}</div>
+  ));
   return (
     //https://s3-us-west-2.amazonaws.com/s.cdpn.io/85486/wave.svg
     <div className="beware-container">
       <div className="title-question">QUESTION ?</div>
       <div className="beware-bottom">
-        <div className="beware-answers">
-          <div>ANSWER 1</div>
-          <div>ANSWER 2</div>
-          <div>ANSWER 3</div>
-        </div>
+        <div className="beware-answers">{listAnswers}</div>
         <div className="beware-validate">
           <button>VALIDATE</button>
         </div>

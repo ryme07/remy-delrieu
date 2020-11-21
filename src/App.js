5baec79f5581080ef4118ba3a089d5aa6a0ed317
ryme07 <remydelrieu@hotmail.fr>
2020-11-21 14:32:00 +0100

    feat: delete useState in APP

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./Style/App.scss";
-import React, { useState } from "react";
+import React from "react";
 import ReactPageScroller from "react-page-scroller";
 import Home from "./Views/Home";
 import Project from "./Views/Project";
@@ -9,9 +9,7 @@ import DsiPE from "./Components/DsiPE";
 import Beware from "./Components/Beware";
 
 function App() {
-  let titletest = "DSI POLE EMPLOI";
-
-  const [projects, setProjects] = useState([
+  const projects = [
     [
       {
         title: "DSI POLE EMPLOI",
@@ -42,7 +40,7 @@ function App() {
         cardRenderer: <Hackstar />,
       },
     ],
-  ]);
+  ];
 
   /*
 it doesn't work :(
@@ -61,19 +59,9 @@ it doesn't work :(
     <React.Fragment>
       <ReactPageScroller>
         <Home />
-
-        <Project
-          projects={projects[0]}
-          // dsi={<DsiPE />}
-        />
-        <Project
-          projects={projects[1]}
-          // hello="Beware"
-        />
-        <Project
-          projects={projects[2]}
-          // stars={<Hackstar />}
-        />
+        <Project projects={projects[0]} />
+        <Project projects={projects[1]} />
+        <Project projects={projects[2]} />
         <About />
       </ReactPageScroller>
     </React.Fragment>

b07ccb109755cf0d828f5611475a5aef3c87f371
ryme07 <remydelrieu@hotmail.fr>
2020-12-08 18:30:03 +0100

    feat: add particles in hackstars

--- src/Components/Hackstars.js
+++ src/Components/Hackstars.js
@@ -1,20 +1,136 @@
 import React from "react";
 import { Zoom } from "react-awesome-reveal";
 import "../Style/Hackstars.scss";
+import Particles from "react-particles-js";
 
 export default function Hackstars() {
   return (
     <Zoom direction="bottom" delay={1100} duration={2000}>
-      <div className="solar-system">
-        <div className="planet planet1">
-          <div className="planet planet2">
-            <div className="planet planet3">
-              <div className="planet planet4">
-                <div className="sun"></div>
+      <div className="testing">
+        <div className="solar-system">
+          <div className="planet planet1">
+            <div className="planet planet2">
+              <div className="planet planet3">
+                <div className="planet planet4">
+                  <div className="sun"></div>
+                </div>
               </div>
             </div>
           </div>
         </div>
+        <Particles
+          id="particles-js"
+          params={{
+            particles: {
+              number: {
+                value: 400,
+                density: {
+                  enable: true,
+                  value_area: 500,
+                },
+              },
+              color: {
+                value: "#ffffff",
+              },
+              shape: {
+                type: "circle",
+                stroke: {
+                  width: 0,
+                  color: "#000000",
+                },
+                polygon: {
+                  nb_sides: 5,
+                },
+                image: {
+                  src: "img/github.svg",
+                  width: 100,
+                  height: 100,
+                },
+              },
+              opacity: {
+                value: 1,
+                random: true,
+                anim: {
+                  enable: true,
+                  speed: 0.2,
+                  opacity_min: 0,
+                  sync: false,
+                },
+              },
+              size: {
+                value: 3,
+                random: true,
+                anim: {
+                  enable: false,
+                  speed: 0.1,
+                  size_min: 0.3,
+                  sync: false,
+                },
+              },
+              line_linked: {
+                enable: false,
+                distance: 150,
+                color: "#ffffff",
+                opacity: 0.4,
+                width: 1,
+              },
+              move: {
+                enable: true,
+                speed: 0.1,
+                direction: "none",
+                random: false,
+                straight: false,
+                out_mode: "out",
+                bounce: false,
+                attract: {
+                  enable: false,
+                  rotateX: 600,
+                  rotateY: 600,
+                },
+              },
+            },
+            interactivity: {
+              detect_on: "canvas",
+              events: {
+                onhover: {
+                  enable: false,
+                  mode: "bubble",
+                },
+                onclick: {
+                  enable: false,
+                  mode: "repulse",
+                },
+                resize: true,
+              },
+              modes: {
+                grab: {
+                  distance: 400,
+                  line_linked: {
+                    opacity: 1,
+                  },
+                },
+                bubble: {
+                  distance: 250,
+                  size: 0,
+                  duration: 2,
+                  opacity: 0,
+                  speed: 3,
+                },
+                repulse: {
+                  distance: 400,
+                  duration: 0.4,
+                },
+                push: {
+                  particles_nb: 4,
+                },
+                remove: {
+                  particles_nb: 2,
+                },
+              },
+            },
+            retina_detect: true,
+          }}
+        />
       </div>
     </Zoom>
   );

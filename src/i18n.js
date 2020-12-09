df4d027209cd64a2cfca6a8ec5ae407402f072d8
ryme07 <remydelrieu@hotmail.fr>
2020-12-09 17:19:08 +0100

    fix: problem with langages & gh-pages

--- src/i18n.js
+++ src/i18n.js
@@ -1,14 +1,26 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import Backend from "i18next-http-backend";
+import translationEN from "./locales/en/translation.json";
+import translationFR from "./locales/fr/translation.json";
+const resources = {
+  en: {
+    translation: translationEN,
+  },
+  fr: {
+    translation: translationFR,
+  },
+};
 
 i18n
   .use(Backend)
   .use(initReactI18next)
 
   .init({
-    lng: "en",
     fallbackLng: "en",
+    debug: true,
+    lng: "en",
+    resources,
     interpolation: {
       escapeValue: false,
     },
